2b073d336c3f4b093a9b79cdddedc7465bb097a5
Chukwuebuka <azukachukwuebuka07@gmail.com>
2022-04-09 18:40:34 +0100

    register page

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/login", function(req,res){
     res.render("login")
 })
 
+app.get("/register", function(req,res){
+    res.render("register")
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, function(){
     console.log("server running on port "+ PORT)
