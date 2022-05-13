b596f18ce6ea677810d0fb479b105844e0f33be1
Chukwuebuka <azukachukwuebuka07@gmail.com>
2022-05-13 04:49:00 +0100

    node-cron fix : 5=> testing previous working cron w/out the email

--- modules/node-cron.js
+++ modules/node-cron.js
@@ -1,4 +1,5 @@
 const cron = require('node-cron')
+// note npm i cron is the real cron task, use that if this step doesnt work
 const { transporter, Message } = require('./nodemailer')
 const {USER, SHORTINVS, CYCLESINVS} = require('./userDB')
 require('dotenv').config()
@@ -20,12 +21,6 @@ const updateShortPayment = async function(){
 }
 
 const updateRunningCycle = async function(){
-    // let message = new Message(process.env.DEV_EMAIL,
-    //     'CRON SCHEDULE HAS RAN FOR CYCLE', `
-    //     cron shedule has update for cyc;es investments, now go check if the functions did run
-    //     `
-    //     )
-    //     transporter.sendMail(message, function(e,d){console.log(d)})
     let readyDocs = await CYCLESINVS.find({active : true, days2run : {$lt : 5}})
     for(i = 0; i <readyDocs.length; i++){
         await CYCLESINVS.updateOne({_id :readyDocs[i]._id}, {
@@ -45,6 +40,7 @@ const updateRunningCycle = async function(){
 let Task = cron.schedule('0 0 0 * * *',function(e){
     try {
         updateShortPayment()
+        updateRunningCycle()
     }
     catch(err){
         let message = new Message(process.env.DEV_EMAIL,
@@ -64,26 +60,5 @@ let Task = cron.schedule('0 0 0 * * *',function(e){
   scheduled: true
 })
 
-let CyclesTask = cron.schedule('0 0 1 * * *', function(){
-    try {
-        updateRunningCycle()
-    }catch(err){
-        let message = new Message(process.env.DEV_EMAIL,
-            'URGENT!! TEMENOS GLOBAL ERROR',
-            `this is an error placed by you. 
-            it happened in the cron file, 
-            while it was trying to update the user investments that are due`,
-            `<h3>this is an error placed by you</h3>
-            <h4> it happened in the cron file </h4>
-            <p> while it was trying to update the user investments that are due</p>
-            <p> the error is : <span style="color: red;">${err.messsage}</span> </p>
-            `
-            )
-            transporter.sendMail(message, function(e,d){console.log(d)})
-        }
-}, {scheduled : true})
-
 Task.start()
-CyclesTask.start()
-
 
